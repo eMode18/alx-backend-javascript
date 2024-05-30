6efe401b88d03403499b5ac1bfb8bd0ae487dc73
eMode18 <ewagandi@gmail.com>
2024-05-30 10:02:15 +0000

    0-promise.js corrections

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 // this function returns a Promise
 function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			const data = { message: "API data response"};
-			resolve(data);}, 2000);
-		});
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { message: "API data response"};
+      resolve(data);}, 2000);
+    });
 }
 
 export default getResponseFromAPI;
